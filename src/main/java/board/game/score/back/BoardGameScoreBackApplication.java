b66b0fb161c949f6d610f342efab0f5ff2c4d02d
Elise <elise.trehout@gmail.com>
2023-03-22 15:32:29 +0100

    Init

---
+++ src/main/java/board/game/score/back/BoardGameScoreBackApplication.java
@@ -0,0 +1,13 @@
+package board.game.score.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameScoreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameScoreBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/board/game/score/back/BoardGameScoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package board.game.score.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameScoreBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
